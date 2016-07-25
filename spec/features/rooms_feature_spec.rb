d9b51d575179fb6e1bfc1bea9f0be368930a2fbc
Ben Stein <bumstain@gmail.com>
2016-07-25 21:27:22 +1000

    Page displays no listings when no rooms are in database

---
+++ spec/features/rooms_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'rooms' do
+  context 'no rooms have been added' do
+    scenario 'should display a prompt to add a room' do
+      visit '/rooms'
+      expect(page).to have_content 'No rooms yet!'
+      expect(page).to have_link 'Add a room now!'
+    end
+  end
+end
