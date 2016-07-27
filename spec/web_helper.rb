a5558ea07b715dbd849281722cf02d07a8fdcc38
Ben Stein <bumstain@gmail.com>
2016-07-27 19:58:44 +1000

    Added TravisCI for build

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,3 +6,11 @@ def sign_up
   fill_in('Password confirmation', with: 'Password1')
   click_button('Sign up')
 end
+
+def log_in
+  visit('/')
+  click_link('Log In')
+  fill_in('Email', with: 'user@example.com')
+  fill_in('Password', with: 'Password1')
+  click_button('Log In')
+end
