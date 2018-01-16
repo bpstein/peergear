bbf032fec112669ff051e55dce2ae6af4f5348c0
Ben Stein <bumstain@gmail.com>
2018-01-17 01:04:44 +0900

    Resolved errors relating to postgres and jquery-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require jquery-ui/datepicker
+//= require jquery-ui/widgets/datepicker
 //= require toastr
 //= require_tree .
